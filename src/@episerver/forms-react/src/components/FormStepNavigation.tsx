0ea3d745424e1d3f01eb347a8a64fb765b175b7e
EP\qang2 <quocanh.nguyen@episerver.com>
2023-12-15 16:48:38 +0700

    Add submit to form steps Fixes: AFORM-3719

--- src/@episerver/forms-react/src/components/FormStepNavigation.tsx
+++ src/@episerver/forms-react/src/components/FormStepNavigation.tsx
@@ -1,22 +1,23 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useForms, useFormsDispatch } from "../context/store";
 import { FormCache, FormConstants, FormContainer, FormStep, StepDependCondition, SubmitButtonType, isNull } from "@episerver/forms-sdk";
 import { DispatchFunctions } from "../context/dispatchFunctions";
-import { useHistory } from "react-router-dom";
 
 interface FormStepNavigationProps {
+    stepLocalizations: Record<string, string>
+    form: FormContainer
     isFormFinalized: boolean
     history?: any
+    handleSubmit: (e: any) => void
 }
 
 export const FormStepNavigation = (props: FormStepNavigationProps) => {
-    const formContext = useForms();
-    const formCache = new FormCache();
-    const form = formContext?.formContainer ?? {} as FormContainer;
-    const depend = new StepDependCondition(form, formContext?.dependencyInactiveElements ?? []);
-    const { isFormFinalized, history } = props;
+    const formContext = useForms()
+    const formCache = new FormCache()
+    const history = props.history
+    const depend = new StepDependCondition(props.form, formContext?.dependencyInactiveElements ?? [])
+    const { stepLocalizations, form, isFormFinalized, handleSubmit } = props;
     const dispatchFuncs = new DispatchFunctions();
-    const stepLocalizations = useRef<Record<string, string>>(form.steps?.filter(s => !isNull(s.formStep.localizations))[0]?.formStep.localizations);
 
     const submittable = true
     const stepCount = form.steps.length;
@@ -36,6 +37,7 @@ export const FormStepNavigation = (props: FormStepNavigationProps) => {
 
     const handleNextStep = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault()
+        handleSubmit(event)
         goToStep(depend.findNextStep(currentStepIndex) ?? 0)
     }
 
@@ -63,13 +65,13 @@ export const FormStepNavigation = (props: FormStepNavigationProps) => {
                         disabled={prevButtonDisableState}
                         onClick={(event) => handlePrevStep(event)}
                     >
-                        {stepLocalizations.current["previousButtonLabel"]}
+                        {stepLocalizations["previousButtonLabel"]}
                     </button>
 
                     <div className="Form__NavigationBar__ProgressBar">
                         <div className="Form__NavigationBar__ProgressBar--Progress" style={{ width: progressWidth }}></div>
                         <div className="Form__NavigationBar__ProgressBar--Text">
-                            <span className="Form__NavigationBar__ProgressBar__ProgressLabel">{stepLocalizations.current["pageButtonLabel"]}</span>
+                            <span className="Form__NavigationBar__ProgressBar__ProgressLabel">{stepLocalizations["pageButtonLabel"]}</span>
                             <span className="Form__NavigationBar__ProgressBar__CurrentStep">{currentDisplayStepIndex}</span>/
                             <span className="Form__NavigationBar__ProgressBar__StepsCount">{stepCount}</span>
                         </div>
@@ -83,7 +85,7 @@ export const FormStepNavigation = (props: FormStepNavigationProps) => {
                         disabled={nextButtonDisableState}
                         onClick={handleNextStep}
                     >
-                        {stepLocalizations.current["nextButtonLabel"]}
+                        {stepLocalizations["nextButtonLabel"]}
                     </button>
                 </nav>
             }
